10803123af172e4aa4836c60f41d63786a0c7cf6
Ahmed El-Sayed Ali <hatemido1122@gmail.com>
2021-05-17 00:10:16 +0200

    fix stack ui

--- lib/home.dart
+++ lib/home.dart
@@ -3,85 +3,81 @@ import 'package:flutter/material.dart';
 import 'package:movie_app/movie.dart';
 
 import 'build_text.dart';
+import 'movie.dart';
 import 'movie_card.dart';
 
-
 class Home extends StatefulWidget {
-
   @override
   _HomeState createState() => _HomeState();
 }
 
 class _HomeState extends State<Home> {
-
-  PageController pageController=PageController();
-  int backgroundIndex=0;
-  void fun()=>print("hhh");
-
+  PageController pageController = PageController();
+  int backgroundIndex = 0;
+  void fun() => print("hhh");
 
   @override
   void initState() {
-   pageController=PageController(
-       viewportFraction: .8,
-     initialPage: 0,
-   )..addListener(fun);
+    pageController = PageController(
+      viewportFraction: .8,
+      initialPage: 0,
+    )..addListener(fun);
   }
-
 
   @override
   Widget build(BuildContext context) {
     Size size = MediaQuery.of(context).size;
-    Movie movie= getMovie[backgroundIndex];
+    Movie movie = getMovie[backgroundIndex];
     int topRate = 10;
 
     return Scaffold(
-        body:Stack(
-          children: [
-            buildBackgroundImage(movie.imageBlur,size),
-            buildToolbar(size),
-            buildPager(size,backgroundIndex),
-            buildMovieText(movie.name,movie.rate,size,movie.description),
-          ],
-        )
-    );
-  }
-  Widget buildBackgroundImage(String img, Size size){
-    return Container(
-        height:size.height,
-        width:size.width,
-        child: Image.asset(
-            img,fit:BoxFit.cover));
+        body: Stack(
+      children: [
+        buildBackgroundImage(movie.imageBlur, size),
+        buildbody(size, getMovie[backgroundIndex])
+      ],
+    ));
   }
 
-  Widget buildToolbar(Size size ){
+  Widget buildBackgroundImage(String img, Size size) {
+    return Container(
+        height: size.height,
+        width: size.width,
+        child: Image.asset(img, fit: BoxFit.cover));
+  }
+
+  Widget buildToolbar(Size size) {
     return Padding(
-      padding: const EdgeInsets.only(top:20.0),
+      padding: const EdgeInsets.only(top: 20.0),
       child: SizedBox(
         height: 150, // TODO: use screen util
         child: InkWell(
-          onTap: (){},
+          onTap: () {},
           child: Row(
             children: [
-              SizedBox(width: 20,),
+              SizedBox(
+                width: 20,
+              ),
               Row(
                   mainAxisAlignment: MainAxisAlignment.center,
                   crossAxisAlignment: CrossAxisAlignment.center,
-                  children:[
-                    Text('Top Rated',  //TODO: use BuildText
-                        style:TextStyle(
-                        color:Colors.white,
-                        fontSize: 30,
-                        fontWeight:FontWeight.bold
-                    )),
-                    Icon(Icons.arrow_drop_down_rounded,color: Colors.white,size: 30,)
+                  children: [
+                    Text('Top Rated', //TODO: use BuildText
+                        style: TextStyle(
+                            color: Colors.white,
+                            fontSize: 30,
+                            fontWeight: FontWeight.bold)),
+                    Icon(
+                      Icons.arrow_drop_down_rounded,
+                      color: Colors.white,
+                      size: 30,
+                    )
                   ]),
-
               Spacer(),
-              IconButton(
-                  onPressed: (){},
-                  icon: Icon(Icons.search)
+              IconButton(onPressed: () {}, icon: Icon(Icons.search)),
+              SizedBox(
+                width: 20,
               ),
-              SizedBox(width: 20,),
             ],
           ),
         ),
@@ -89,77 +85,99 @@ class _HomeState extends State<Home> {
     );
   }
 
-  Widget buildPager( Size size , int page  ){
+  Widget buildPager(Size size, int page) {
     return Container(
+      height: size.height * .6,
       child: PageView.builder(
-        onPageChanged:  (pageIndex){
+        onPageChanged: (pageIndex) {
           setState(() {
-            backgroundIndex= pageIndex;
+            backgroundIndex = pageIndex;
           });
         },
         controller: pageController,
-        itemBuilder: (context,index)=>
-            MovieCard(getMovie[index]),
+        itemBuilder: (context, index) => MovieCard(getMovie[index]),
         itemCount: getMovie.length,
       ),
     );
   }
-  Widget buildMovieText(String movieName,double rate,Size size,String description){
-    return Positioned(
-      bottom: size.height*.20 -120, //TODO: use the package
-      child: Container(
-        margin: EdgeInsets.symmetric(horizontal: 10,vertical: 3),
-        width:size.width-10,
-        padding:const EdgeInsets.only(top: 25,left: 20, right: 20),
-        child: Column(
-          children: [
-            Row(
-              children: [
-                SizedBox(width: 20,),
-                AppText(text: movieName,fontWeight: FontWeight.bold,textSize: 30,),
-                Spacer(),
-                AppText(text: rate.toString(),fontWeight: FontWeight.bold,textSize: 20,),
-                AppText(text: '/10',textSize: 20,color: Colors.white.withOpacity(.5),),
-                SizedBox(width: 20,),
 
-              ],
-            ),
-            SizedBox(height: 20,),
-            Container(
-                margin: EdgeInsets.only(left: 20, right: 20),
-                child: AppText(text: description))
-          ],
-        ),
+  Widget buildMovieText(
+      String movieName, double rate, Size size, String description) {
+    return Container(
+      margin: EdgeInsets.symmetric(horizontal: 10, vertical: 3),
+      child: Column(
+        children: [
+          Row(
+            children: [
+              SizedBox(
+                width: 20,
+              ),
+              AppText(
+                text: movieName,
+                fontWeight: FontWeight.bold,
+                textSize: 30,
+              ),
+              Spacer(),
+              AppText(
+                text: rate.toString(),
+                fontWeight: FontWeight.bold,
+                textSize: 20,
+              ),
+              AppText(
+                text: '/10',
+                textSize: 20,
+                color: Colors.white.withOpacity(.5),
+              ),
+              SizedBox(
+                width: 20,
+              ),
+            ],
+          ),
+          SizedBox(
+            height: 20,
+          ),
+          Container(
+              margin: EdgeInsets.only(left: 20, right: 20),
+              child: AppText(text: description))
+        ],
       ),
     );
   }
 
-
-
-
   List<Movie> getMovie = [
-    Movie('MoonLight',
+    Movie(
+        'MoonLight',
         'assets/img1.jpeg',
         'assets/back1.jpeg',
         'The Golden Gate Bridge '
             'is a suspension bridge spanning the Golden Gate,'
             ' the one-mile-wide strait connecting San Francisco Bay and the Pacific Ocean.',
         7.5),
-    Movie('Joker',
+    Movie(
+        'Joker',
         'assets/img4.jpeg',
         'assets/back4.jpeg',
         'The Golden Gate Bridge '
             'is a suspension bridge spanning the Golden Gate,'
             ' the one-mile-wide strait connecting San Francisco Bay and the Pacific Ocean.',
         7.2),
-    Movie('Logan',
+    Movie(
+        'Logan',
         'assets/img3.jpeg',
         'assets/video2.jpeg',
         'DecorView setVisiblity'
             'DecorView setVisiblityDecorView setVisiblity'
             'DecorView setVisiblityDecorView setVisiblity',
         7.3),
-
   ];
-  }
 
+  Widget buildbody(Size size, Movie movie) {
+    return Column(
+      children: [
+        buildToolbar(size),
+        buildPager(size, backgroundIndex),
+        buildMovieText(movie.name, movie.rate, size, movie.description),
+      ],
+    );
+  }
+}
--- lib/movie_card.dart
+++ lib/movie_card.dart
@@ -4,110 +4,130 @@ import 'build_text.dart';
 import 'movie.dart';
 
 class MovieCard extends StatelessWidget {
-  Movie movie ;
+  Movie movie;
   MovieCard(this.movie);
 
   @override
   Widget build(BuildContext context) {
-   // PageController pageController=PageController(viewportFraction: .8);
+    // PageController pageController=PageController(viewportFraction: .8);
     Size size = MediaQuery.of(context).size;
-    var cardWidth = size.width - 70;
-    var cardHeight = size.height*.65;
+    var cardWidth = size.width * .8;
+    var cardHeight = size.height * .65;
+    return buildMovieImage(cardWidth, cardHeight, size, movie.imageTop);
     return Stack(
       children: [
-       /// buildBackgroundImage(movie.imageBlur,size),
+        /// buildBackgroundImage(movie.imageBlur,size),
         ///buildToolbar(size),
-        buildMovieImage(cardWidth,cardHeight,size,movie.imageTop),
-       /// buildMovieText(movie.name,movie.rate,size,movie.description),
+        buildMovieImage(cardWidth, cardHeight, size, movie.imageTop),
 
+        /// buildMovieText(movie.name,movie.rate,size,movie.description),
       ],
     );
   }
-  Widget buildBackgroundImage(String img, Size size){
+
+  Widget buildBackgroundImage(String img, Size size) {
     return Container(
-        height:size.height,
-        width:size.width,
-        child: Image.asset(
-            img,fit:BoxFit.cover));
+        height: size.height,
+        width: size.width,
+        child: Image.asset(img, fit: BoxFit.cover));
   }
-  Widget buildToolbar(Size size ){
+
+  Widget buildToolbar(Size size) {
     return Padding(
-      padding: const EdgeInsets.only(top:20.0),
+      padding: const EdgeInsets.only(top: 20.0),
       child: SizedBox(
         height: 150,
         child: InkWell(
-          onTap: (){},
+          onTap: () {},
           child: Row(
             children: [
-              SizedBox(width: 20,),
-              Row(
-               mainAxisAlignment: MainAxisAlignment.center,
-               crossAxisAlignment: CrossAxisAlignment.center,
-               children:[
-                 Text('Top Rated', style:TextStyle(
-                     color:Colors.white,
-                     fontSize: 30,
-                     fontWeight:FontWeight.bold
-                 )),
-                 Icon(Icons.arrow_drop_down_rounded,color: Colors.white,size: 30,)
-                 ]),
-
-               Spacer(),
-              IconButton(
-                  onPressed: (){},
-                  icon: Icon(Icons.search)
+              SizedBox(
+                width: 20,
               ),
-              SizedBox(width: 20,),
+              Row(
+                  mainAxisAlignment: MainAxisAlignment.center,
+                  crossAxisAlignment: CrossAxisAlignment.center,
+                  children: [
+                    Text('Top Rated',
+                        style: TextStyle(
+                            color: Colors.white,
+                            fontSize: 30,
+                            fontWeight: FontWeight.bold)),
+                    Icon(
+                      Icons.arrow_drop_down_rounded,
+                      color: Colors.white,
+                      size: 30,
+                    )
+                  ]),
+              Spacer(),
+              IconButton(onPressed: () {}, icon: Icon(Icons.search)),
+              SizedBox(
+                width: 20,
+              ),
             ],
           ),
         ),
       ),
     );
   }
-  Widget buildMovieImage(double cardWidth, double cardHeight, Size size,String img){
-    return Positioned(
-      bottom: size.height*.20, //TODO:
-      child: Container(
-        margin: EdgeInsets.symmetric(horizontal: 20),
-          width:cardWidth ,
-          height: cardHeight,
-        child: Image.asset(img,fit: BoxFit.fitWidth,),
 
-
+  Widget buildMovieImage(
+      double cardWidth, double cardHeight, Size size, String img) {
+    return Container(
+      margin: EdgeInsets.symmetric(horizontal: 20),
+      width: cardWidth,
+      height: cardHeight,
+      child: Image.asset(
+        img,
+        fit: BoxFit.cover,
       ),
     );
-
   }
-  Widget buildMovieText(String movieName,double rate,Size size,String description){
+
+  Widget buildMovieText(
+      String movieName, double rate, Size size, String description) {
     return Positioned(
-      bottom: size.height*.20 -100,//TODO
+      bottom: size.height * .20 - 100, //TODO
       child: Container(
-        width:size.width,
-        padding:const EdgeInsets.only(top: 20),
+        width: size.width,
+        padding: const EdgeInsets.only(top: 20),
         child: Column(
           children: [
             Row(
               children: [
-               SizedBox(width: 20,),
-                AppText(text: movieName,fontWeight: FontWeight.bold,textSize: 30,),
+                SizedBox(
+                  width: 20,
+                ),
+                AppText(
+                  text: movieName,
+                  fontWeight: FontWeight.bold,
+                  textSize: 30,
+                ),
                 Spacer(),
-                AppText(text: rate.toString(),fontWeight: FontWeight.bold,textSize: 20,),
-                AppText(text: '/10',textSize: 20,color: Colors.white.withOpacity(.5),),
-                SizedBox(width: 20,),
-
+                AppText(
+                  text: rate.toString(),
+                  fontWeight: FontWeight.bold,
+                  textSize: 20,
+                ),
+                AppText(
+                  text: '/10',
+                  textSize: 20,
+                  color: Colors.white.withOpacity(.5),
+                ),
+                SizedBox(
+                  width: 20,
+                ),
               ],
             ),
-            SizedBox(height: 30,),
+            SizedBox(
+              height: 30,
+            ),
             Container(
-              margin: EdgeInsets.only(left: 20, right: 20),
+                margin: EdgeInsets.only(left: 20, right: 20),
                 child: AppText(text: description))
           ],
         ),
       ),
     );
   }
-
 }
-
-
-
